1261194b22a4d561d024e04fa47692a3f9e8b7af
Juliano Ferreira Rodrigues <46505899+julianofrodrigues@users.noreply.github.com>
2020-07-28 15:10:29 -0300

    att home, config routes cadastro video & categoria

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Video from './pages/Cadastro/Video';
+import Categoria from './pages/Cadastro/Categoria';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/cadastro-video" component={Video} />
+      <Route path="/cadastro-categoria" component={Categoria} />
+      <Route component={()=>(<>404</>)}  />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
